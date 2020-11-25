81b6030cfe1be9a232dbf429c2adf600c1a6e37c
Kevin Lee <jinpyol2@illinois.edu>
2020-11-25 16:03:59 -0600

    read from file done

--- degree.cpp
+++ degree.cpp
@@ -25,8 +25,38 @@ void degree::readFromData()
         }
         std::string source = routes[3];
         std::string destination = routes[5];
-        _g.insertVertex(source)
 
+        if(!(_g.vertexExists(source)))
+        {   
+            _g.insertVertex(source);
+        }
+        if(!(_g.vertexExists(destination)))
+        {   
+            _g.insertVertex(destination);
+        }
+        if(!(_g.edgeExists(source, destination)))
+        {   
+            _g.insertEdge(source, destination);
+            _g.setEdgeWeight(source, destination, 0);
+        }
+        else{
+            _g.setEdgeWeight(source, destination, getEdgeWeight(source, destination) + 1);
+        }
+
+        if(node_weight.find(source) != node_weight.end())
+        {
+            node_weight[source]++;
+        }
+        else{
+            node_weight[source] = 0;
+        }
+        if(node_weight.find(destination) != node_weight.end())
+        {
+            node_weight[destination]++;
+        }
+        else{
+            node_weight[destination] = 0;
+        }
 
     }    
      
