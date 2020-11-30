f02ad53fe011d6e862d3f67c3de9b2a566ba4f85
Brandon Oh <bo7@linux-a2.ews.illinois.edu>
2020-11-30 11:56:22 -0600

    First commit

--- circle.cpp
+++ circle.cpp
@@ -36,6 +36,7 @@ bool Circle::contains(const Vector2& p) const
 {
     const double distance = this->center().distanceTo(p);
     return distance <= this->radius_;
+    
 }
 
 void Circle::drawPoints(PNG* canvas, int x, int y) const
