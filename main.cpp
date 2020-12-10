8983fb0b07001dc552ee1f00bcae15f90ab620c2
Kevin Lee <jinpyol2@illinois.edu>
2020-12-09 21:26:20 -0600

    command

--- main.cpp
+++ main.cpp
@@ -5,14 +5,31 @@
 int main()
 {
     degree d("routedata.txt", "airports_full.txt");
-    d.readFromData();
-    d.drawOnMap();
-    d.readFromAirport();
-
     BFS bfs("routedata.txt");
-    bfs.saveResult("path.txt", "edges.txt");
-    
     std::unordered_map<std::string, std::pair<double, double>> locations = d.getLocations();
     LandmarkPath lp("routedata.txt", locations);
-    lp.printPath(lp.getResult("BOS", "YRI", "PVG"));
+
+    while(1)
+    {  
+        std::cout<<"Type degree, bfs or landmark"<<'\n';
+        std::string input;
+        std::cin>>input;
+        if(input == "degree")
+        {
+            d.readFromData();
+            d.drawOnMap();
+            d.readFromAirport();
+            std::cout<<"Out.png created"<<'\n';
+        }
+        if(input == "bfs")
+        {
+            bfs.saveResult("path.txt", "edges.txt");
+            std::cout<<'\n';
+        }
+        if(input == "landmark")
+        {
+            lp.printPath(lp.getResult("BOS", "YRI", "PVG"));
+            std::cout<<'\n';
+        }
+    }
 }
